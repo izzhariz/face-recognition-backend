70917c1a9b4b865e1fd2e7d8f5b92acc81f5eaff
IzzHrz <izzharizdev@gmail.com>
2020-10-29 15:53:35 +0800

    moved api key from frontend to backend and fixed user authentication on register/signin

--- controllers/image.js
+++ controllers/image.js
@@ -1,3 +1,19 @@
+const Clarifai = require('clarifai')
+
+const app = new Clarifai.App({
+    apiKey: '4f65e4f902f64fff856ee0036f06926a'
+})
+
+const handleApiCall = (req, res) => {
+    app.models
+    .predict(Clarifai.FACE_DETECT_MODEL, req.body.input)
+    .then(data => {
+        res.json(data)
+    })
+    .catch(err => res.status(400).json('unable to work with API'))
+}
+
+
 const handleImage = (req, res, db) => {
     const { id } = req.body;
     db('users').where('id', '=', id)
@@ -9,4 +25,4 @@ const handleImage = (req, res, db) => {
     .catch(err => res.status(404).json('unable to get entries'))
 }
 
-module.exports = { handleImage }
+module.exports = { handleImage, handleApiCall }
